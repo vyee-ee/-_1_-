535740da940ef31acb28f4949bd4122198ab9a33
Сергей Дмитриевич Кузьминых <seregaokarov@gmail.com>
2024-11-24 16:04:51 +0300

    Create config.py

---
+++ arkanoid_package/config.py
@@ -0,0 +1,6 @@
+'''Этот модуль подгружает необходимые для работы файла'''
+
+
+block_break_sound = pygame.mixer.Sound('clash-royale-laugh.mp3')  # Replace with your actual block break sound file path
+background_music = pygame.mixer.Sound('C418_-_Subwoofer_Lullaby_30921632.mp3')  # Replace with your actual background music file path
+background_music.set_volume(0.5)
